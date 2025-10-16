9c1e232084d2a900b9ab76cc50031a337f94c617
Bing Chilling <119095020+AlvinT-data@users.noreply.github.com>
2025-10-16 10:31:49 -0700

    Fix TRIM function usage in load_geolocation.sql

--- sql_scripts/silver/load_geolocation.sql
+++ sql_scripts/silver/load_geolocation.sql
@@ -18,9 +18,9 @@ INSERT INTO silver.geolocation (
 	geolocation_state
 )
 SELECT 
-	TRIM(geolocation_zip_code_prefix) AS zip_code_prefix,
+	TRIM('"' FROM geolocation_zip_code_prefix) AS zip_code_prefix,
 	ROUND(AVG(geolocation_lat), 3) AS latitude,
 	ROUND(AVG(geolocation_lng), 3) AS longitude,
 	MIN(geolocation_state) AS geolocation_state
 FROM bronze.geolocation
-GROUP BY TRIM(geolocation_zip_code_prefix)
+GROUP BY TRIM('"' FROM geolocation_zip_code_prefix)
